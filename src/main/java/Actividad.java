b45067c3168e55ef5696884ebccdb0b40e2661e1
sergiolix <sergiolix8@gmail.com>
2021-03-18 08:27:18 -0500

    se agrego comentario

--- src/main/java/Actividad.java
+++ src/main/java/Actividad.java
@@ -79,7 +79,7 @@ public class Actividad extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    JOptionPane.showMessageDialog(this,"hola como se encuentran el dia de hoy");        // TODO add your handling code here:
+    JOptionPane.showMessageDialog(this,"que se hace hoy?");        // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
